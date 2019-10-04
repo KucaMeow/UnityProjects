9c16447b30cfb89a67aead11dbab1073ca5c3b57
Stepan Ponomarev <stepan@ponomarev.pp.ru>
2019-10-04 18:21:10 +0300

    FPS controller added

---
+++ ShooterTemplate/Assets/Characters/FPS_Player/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransorm;
+    [SerializeField] private float sensetivity = 100;
+    private Transform cameraTransform;
+    private float xAxis;
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        xAxis = 0f;
+    }
+    void Start()
+    {
+        cameraTransform = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        //horisontal rotation
+        float mouseX = Input.GetAxis("Mouse X") * sensetivity * Time.deltaTime;
+        var rotation = playerTransorm.eulerAngles;
+        rotation.y += mouseX;
+        playerTransorm.eulerAngles = rotation;
+        
+        //vertical rotation
+        float mouseY = Input.GetAxis("Mouse Y") * sensetivity * Time.deltaTime;
+        xAxis += mouseY;
+
+        if(xAxis > 90f)
+        {
+            xAxis = 90f;
+            mouseY = 0f;
+            setVertRotation(270f);
+        }
+        else if(xAxis < -90f)
+        {
+            xAxis = -90f;
+            mouseY = 0f;
+            setVertRotation(90f);
+        }
+
+        cameraTransform.Rotate(Vector3.left * mouseY);
+    }
+
+    private void setVertRotation (float angle)
+    {
+        Vector3 eulerRot = cameraTransform.eulerAngles;
+        eulerRot.x = angle;
+        transform.eulerAngles = eulerRot;
+    }
+}
---
+++ ShooterTemplate/Assets/Characters/FPS_Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{    
+    private CharacterController cc;
+    private Transform t;
+    [SerializeField] private float speed = 300;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        t = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        Vector3 forward = t.forward;
+        Vector3 movement_speed = (
+            // Vector3.forward * Input.GetAxis("Vertical") +
+            // Vector3.right * Input.GetAxis("Horizontal")
+            forward * Input.GetAxis("Vertical") +
+            Vector3.Cross(forward, Vector3.up) * Input.GetAxis("Horizontal")
+            ) * Time.deltaTime * speed;
+        
+        cc.SimpleMove(movement_speed);
+    }
+}
