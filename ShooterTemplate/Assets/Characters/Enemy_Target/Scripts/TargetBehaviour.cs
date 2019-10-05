c98fc335441efdc47ddff8151ecec32f68406dda
Stepan Ponomarev <stepan@ponomarev.pp.ru>
2019-10-05 11:16:41 +0300

    Added shooting and targets

---
+++ ShooterTemplate/Assets/Characters/Enemy_Target/Scripts/TargetBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour
+{
+    private int HP = 10;
+    private Transform playerTr;
+    void Start()
+    {
+        playerTr = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Debug.Log(GameObject.FindGameObjectWithTag("Player").name);
+    }
+    void Update()
+    {
+        GetComponent<Transform>().LookAt(playerTr);
+    }
+
+    public void hitEvent()
+    {
+        if (HP > 0)
+        {
+            HP--;
+            Debug.Log(HP);
+        }
+        else
+        {
+            Debug.Log("Target should die");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ ShooterTemplate/Assets/Characters/FPS_Player/Scripts/RayShooter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private Object hitSphere;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            Vector3 center = new Vector3 (cam.pixelWidth/2, cam.pixelHeight/2, 0);
+            Ray shootRay = cam.ScreenPointToRay(center);
+            RaycastHit hit;
+            if  (Physics.Raycast(shootRay, out hit))
+            {
+                GameObject hitObj = hit.transform.gameObject;
+                TargetBehaviour tb = hitObj.GetComponent<TargetBehaviour>();
+                if  (tb != null)
+                {
+                    tb.hitEvent();
+                }
+                else
+                {
+                    StartCoroutine(spawnSphere(hit.point));
+                }
+            }
+        }
+    }
+
+    private IEnumerator spawnSphere (Vector3 pos)
+    {
+        GameObject sphere = GameObject.Instantiate((GameObject)hitSphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+}
